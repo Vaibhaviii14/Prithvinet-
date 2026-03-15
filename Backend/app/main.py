9a21934cb5dc35fc9264bc08ad75dad8e6f391c9
Ashutosh Behera <ashupoco66@gmail.com>
2026-03-15 09:05:32 +0530

    crs updated

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -14,9 +14,9 @@ app = FastAPI(title="PrithviNet API 🚀")
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"], 
+    allow_origins=["*"],  # The "*" means "Allow requests from ANY website"
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["*"],  # Allow POST, GET, PUT, DELETE
     allow_headers=["*"],
 )
 
